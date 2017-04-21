b0b7f7beaf87b6b7e385138ed0eeeb0ea69b9e87
n0npax <marcin@niemira.net>
2017-04-21 23:03:07 +0200

    add some flags

--- src/bin/cal.rs
+++ src/bin/cal.rs
@@ -34,6 +34,15 @@ OPTIONS
    -3, --three
       Display three months spanning the date.
 
+   -n , --months number
+        Display number of months, starting from the month containing the date.
+
+   -y, --year
+        Display a calendar for the whole year.
+
+   -Y, --twelve
+        Display a calendar for the next twelve months.
+
    -monday
        monday as first day of week
 
@@ -43,6 +52,8 @@ OPTIONS
     -h
     --help
         display this help and exit
+AUTHOR
+    Marcin Niemira
 "#; /* @MANEND */
 
 #[derive(Debug, Copy, Clone)]
@@ -75,6 +86,8 @@ impl CalendarDay {
             leap: leap,
             weekday: -1,
         };
+        some_date.shift_month(0); //validate
+        some_date.shift_day(0);
         some_date.update_weekday();
         some_date
     }
@@ -96,7 +109,7 @@ impl CalendarDay {
                 self.year -= 1;
             }
             1...12 => {}
-            _ => panic!("month shift failed"),
+            _ => panic!("month error {:?}", self),
         }
         self.update_weekday();
     }
@@ -121,8 +134,10 @@ impl CalendarDay {
                 weekday: -1,
             };
             self.day = dummy_active_date.get_month_days().len() as i8;
+        } else if self.day == -1 && shift == 0 {
+            // hidden day
         } else if self.day < 0 || self.day > days_in_month {
-            panic!("day shift failed");
+            panic!("day error: {:?}", self);
         }
         self.update_weekday();
     }
@@ -160,11 +175,11 @@ impl CalendarDay {
         let t: [i64; 12] = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4];
         let mut dy = y;
         dy -= match m {
-            0|1|2 => 1,
-            _ => 0
+            0 | 1 | 2 => 1,
+            _ => 0,
         };
         // works fine for yar > 1752
-        self.weekday = ((dy + dy/4 - dy/100 + dy/400 + t[(m-1) as usize ] + d) % 7) as i8;
+        self.weekday = ((dy + dy / 4 - dy / 100 + dy / 400 + t[(m - 1) as usize] + d) % 7) as i8;
     }
 }
 
@@ -179,47 +194,88 @@ impl CalendarWeek {
     }
 }
 
-fn print_calendar(active_date: CalendarDay, stdout: &mut StdoutLock, stderr: &mut Stderr) {
+fn print_calendar(parser: &ArgParser, active_date: CalendarDay, stdout: &mut StdoutLock, stderr: &mut Stderr) {
+    //let sunday_first = parser.found("sunday");
+    let monday_first = parser.found("monday");
 
-    let mut calendar_string = ["".to_owned(), "".to_owned(), "".to_owned(), "".to_owned(), "".to_owned(), "".to_owned()].to_vec();
-
-
-    //for month_shift in -1..2 {
-    for month_shift in 0..1 {
-        let mut some_day = CalendarDay::new(active_date.year, active_date.month + month_shift, 1);
-
-        let mut cw = CalendarWeek::new();
-        for _ in 0..some_day.weekday {
-            cw.days.push(CalendarDay::new(some_day.year, some_day.month, -1));
-        }
-        for day_num in some_day.get_month_days().iter() {
-            cw.days.push(CalendarDay::new(some_day.year, some_day.month, (*day_num) as i8));
-        }
-        for _ in cw.days.len()..37 {
-            cw.days.push(CalendarDay::new(some_day.year, some_day.month, -1));
-        }
-
-        for (week_num, week) in cw.days.chunks(7).enumerate() {
-            let mut calendar_row = "".to_owned();
-            for day in week {
-                if day.day > 0 {
-                    if day.day == active_date.day && day.month == active_date.month {
-                        calendar_row.push_str(&format!("{}{}", color::Bg(color::White), color::Fg(color::Black)));
-                    }
-                    calendar_row.push_str(&format!("{:>2}", day.day));
-                    calendar_row.push_str(&format!("{}{} ", color::Bg(color::Reset), color::Fg(color::Reset)));
-                } else {
-                    calendar_row.push_str(&format!("{:>1} ", "  "));
-                }
-            }
-            calendar_string[week_num].push_str(&calendar_row);
-            calendar_string[week_num].push_str("\t");
-        }
+    let weekdays_names;
+    let weekdays_complement;
+    if monday_first {
+        weekdays_complement = 1;
+        weekdays_names = "Mo Tu We Th Fr Sa Su";
+    } else {
+        weekdays_complement = 0;
+        weekdays_names = "Su Mo Tu We Th Fr Sa";
     }
-    stdout.write(calendar_string.join("\n").as_bytes()).try(stderr);
-    stdout.write("\n".as_bytes()).try(stderr);
-}
 
+    let mut months_range: Vec<_>;
+    if parser.found("twelve") {
+        months_range = (0..12).collect();
+    } else if parser.found("year") {
+        let active_month = active_date.month;
+        months_range = (-active_month..(12 - active_month)).collect();
+    } else if parser.found("three") {
+        months_range = (-1..2).collect();
+    } else if parser.found("months") {
+        let n = parser.get_opt("n")
+            .unwrap()
+            .parse::<i8>()
+            .unwrap();
+        months_range = (0..n).collect();
+    } else {
+        months_range = (0..1).collect();
+    }
+
+
+    for months_range_max3 in months_range.chunks(3) {
+        let mut calendar_string = ["".to_owned(), "".to_owned(), "".to_owned(), "".to_owned(), "".to_owned(), "".to_owned(), "".to_owned(), "".to_owned()].to_vec();
+        for month_shift in months_range_max3 {
+            //for month_shift in 0..1 {
+            let mut some_day = CalendarDay::new(active_date.year, active_date.month, 1);
+            some_day.shift_month(*month_shift);
+
+            let mut cw = CalendarWeek::new();
+
+            for _ in weekdays_complement..some_day.weekday {
+                cw.days.push(CalendarDay::new(some_day.year, some_day.month, -1));
+            }
+
+            for day_num in some_day.get_month_days().iter() {
+                cw.days.push(CalendarDay::new(some_day.year, some_day.month, (*day_num) as i8));
+            }
+            for _ in cw.days.len()..37 {
+                cw.days.push(CalendarDay::new(some_day.year, some_day.month, -1));
+            }
+
+
+            let month_name = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            calendar_string[0].push_str(&format!("{:>11} {:<12}", month_name[(some_day.month - 1) as usize], some_day.year));
+            calendar_string[1].push_str(&format!("{:<23}", weekdays_names));
+
+
+            for (week_num, week) in cw.days.chunks(7).enumerate() {
+                let mut calendar_row = "".to_owned();
+
+                for day in week {
+                    if day.day > 0 {
+                        if day.day == active_date.day && day.month == active_date.month {
+                            calendar_row.push_str(&format!("{}{}", color::Bg(color::White), color::Fg(color::Black)));
+                        }
+                        calendar_row.push_str(&format!("{:>2}", day.day));
+                        calendar_row.push_str(&format!("{}{} ", color::Bg(color::Reset), color::Fg(color::Reset)));
+                    } else {
+                        calendar_row.push_str(&format!("{:>1} ", "  "));
+                    }
+                }
+                calendar_string[week_num + 2].push_str(&format!("{:<21}", &calendar_row));
+                calendar_string[week_num + 2].push_str("  ");
+            }
+        }
+
+        stdout.write(calendar_string.join("\n").as_bytes()).try(stderr);
+        stdout.write("\n\n".as_bytes()).try(stderr);
+    }
+}
 
 fn get_year_from_args(arg: &String) -> i64 {
     arg.parse::<i64>().expect("not a valid year")
@@ -234,7 +290,6 @@ fn get_day_from_args(arg: &String) -> i64 {
 }
 
 
-
 fn main() {
     let stdout = stdout();
     let mut stdout = stdout.lock();
@@ -242,10 +297,13 @@ fn main() {
 
     let mut parser = ArgParser::new(8)
         .add_flag(&["h", "help"])
-        .add_flag(&["1"])
-        .add_flag(&["3"])
+        .add_flag(&["1", "one"])
+        .add_flag(&["3", "three"])
         .add_flag(&["m", "monday"])
-        .add_flag(&["s", "sunday"]);
+        .add_flag(&["s", "sunday"])
+        .add_flag(&["n", "months"])
+        .add_flag(&["Y", "twelve"])
+        .add_flag(&["y", "year"]);
 
     parser.parse(env::args());
 
@@ -255,6 +313,7 @@ fn main() {
         exit(0);
     }
 
+    //today's date
     let time = SystemTime::now();
     let duration = time.duration_since(UNIX_EPOCH).try(&mut stderr);
     let ts = duration.as_secs() as i64;
@@ -263,7 +322,6 @@ fn main() {
     let (mut year, mut month, mut day, _, _, _) = get_time_tuple(ts, tz_offset);
     if parser.args.len() > 0 {
         match parser.args.len() {
-            0 => {}
             1 => {
                 year = get_year_from_args(&parser.args[0]);
             }
@@ -277,16 +335,14 @@ fn main() {
                 day = get_day_from_args(&parser.args[0]);
             }
             _ => {
-                panic!("To many arguments!.\nUse --help to read man");
+                //    panic!("To many arguments!.\nUse --help to read man");
             }
         }
     }
 
     let active_date = CalendarDay::new(year, month as i8, day as i8);
 
-    //    stdout.write(&format!("{:?}\ni {:?}", get_month_days(active_date),active_date).as_bytes()).try(&mut stderr);
-
-    print_calendar(active_date, &mut stdout, &mut stderr);
+    print_calendar(&parser, active_date, &mut stdout, &mut stderr);
 
 }
 
@@ -351,22 +407,15 @@ fn test_cal_days_in_year() {
 #[test]
 fn test_weekdays() {
     let before_george = CalendarDay::new(1751, 1, 1);
-    let after_george = CalendarDay::new(1753, 1, 1);
+    let after_george = CalendarDay::new(1753, 6, 8);
     let epoch = CalendarDay::new(1970, 1, 1);
     let warsaw_uprising = CalendarDay::new(1944, 8, 1);
     let perestroika = CalendarDay::new(1980, 5, 1);
 
+    //assert_eq!(before_george.weekday, 2);
     //TODO
-}
-
-
-#[test]
-fn test_days_since_epoch() {
-    let before_george = CalendarDay::new(1751, 1, 1);
-    let after_george = CalendarDay::new(1753, 1, 1);
-    let epoch = CalendarDay::new(1970, 1, 1);
-    let warsaw_uprising = CalendarDay::new(1944, 8, 1);
-    let perestroika = CalendarDay::new(1980, 5, 1);
-
-    //TODO
+    assert_eq!(after_george.weekday, 5);
+    assert_eq!(epoch.weekday, 4);
+    assert_eq!(warsaw_uprising.weekday, 2);
+    assert_eq!(perestroika.weekday, 4);
 }
